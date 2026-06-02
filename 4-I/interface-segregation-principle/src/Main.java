3f46d01e63b71b2d1ef62f3f15029b2c2f3fca50
dk-dev01 <derick.hatakeyama@gmail.com>
2026-06-02 14:59:03 -0300

    fix: correcao de escrita

--- 4-I/interface-segregation-principle/src/Main.java
+++ 4-I/interface-segregation-principle/src/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args){
         Livro l1 = new Livro("Biblia Sagrada", "Deus", "Céu", 7);
         Audiobook a1 = new Audiobook("Salmos 23", "Rei Davi", "desconhecido", 1);
-        ArquivoPDF pdf1 = new ArquivoPDF("Provérbios", "Salomão", 100);
+        ArquivoPDF pdf1 = new ArquivoPDF("Provérbios", "Salo�o", 100);
 
         l1.listarItem();
         a1.listarItem();
